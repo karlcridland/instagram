619e1e3008df68ab7ee746d1bd4ec9725bc73cdb
Karl Cridland <karlcridland@gmail.com>
2022-08-12 09:24:53 +0100

    Template for instagram class started.

---
+++ Instagram Followers/Instagram.swift
@@ -0,0 +1,30 @@
+//
+//  Instagram.swift
+//  Instagram Followers
+//
+//  Created by Karl Cridland on 11/08/2022.
+//
+
+import Foundation
+
+class Instagram{
+    
+    public let shared: Instagram = Instagram()
+    
+    private init(){
+        
+    }
+    
+    func getFollowers() -> [String]{
+        return []
+    }
+    
+    func getFollowing() -> [String]{
+        return []
+    }
+    
+    func promptSignIn(){
+        
+    }
+    
+}
